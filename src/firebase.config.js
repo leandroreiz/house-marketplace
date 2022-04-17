b83867127e4e088905831fde70e43419ecc4a372
Leandro Reis <leandro.reiz@gmail.com>
2022-04-17 14:12:59 +0100

    Setup Firebase and Firestore, add basic file structure and routes

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnrxX-NO939xH4ZFxIB5O2Xa_-Pg4YpHI',
+  authDomain: 'house-marketplace-app-e89f9.firebaseapp.com',
+  projectId: 'house-marketplace-app-e89f9',
+  storageBucket: 'house-marketplace-app-e89f9.appspot.com',
+  messagingSenderId: '280768151469',
+  appId: '1:280768151469:web:f62f856ecaeb03bdeb2b24',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
